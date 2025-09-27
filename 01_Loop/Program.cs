caf5077452245c29151dd9277f77d75cf97e43a8
JIEUN-YI <yi34364064@knou.ac.kr>
2025-09-27 17:19:39 +0900

    [Feat] 별그리기 예제
    StringBuilder를 활용하여 반복문으로 별그리기 문제풀이

--- 01_Loop/Program.cs
+++ 01_Loop/Program.cs
@@ -1,10 +1,155 @@
-﻿namespace _01_Loop
+﻿using System.Text;
+
+namespace _01_Loop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Loop loop = new Loop();
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("Number 1");
+            sb.Append(loop.Number1(5));
+            Console.WriteLine("Number 2");
+            sb.Append(loop.Number2(5));
+            Console.WriteLine("Number 3");
+            sb.Append(loop.Number3(5));
+            Console.WriteLine("Number 4");
+            sb.Append(loop.Number4(5));
+            Console.WriteLine(sb.ToString());
+
+        }       
+
+    }
+    /// <summary>
+    /// 반복문 클래스 생성
+    /// </summary>
+    public class Loop
+    {
+        /// <summary>
+        /// StringBuilder에 *을 저장
+        /// </summary>
+        /// <returns></returns>
+        public StringBuilder Drow()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("*");
+            return sb;
+        }
+
+        /// <summary>
+        /// 출력예시
+        /// *
+        /// **
+        /// ***
+        /// ****
+        /// *****
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public string Number1(int line)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Number1\n====================");
+            for (int count = 0; count < line; count++)
+            {
+                for (int star = 1; star <= count + 1; star++)
+                {
+                    sb.Append(Drow());
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 출력예시
+        ///      *
+        ///     **
+        ///    ***
+        ///   ****
+        ///  *****
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public string Number2(int line)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Number2\n====================");
+            for(int y = 0, star = 1; y < line; y++, star++)
+            {
+                for(int x = 0; x < line; x++)
+                {
+                    if (x < line - star)
+                    {
+                        sb.Append(" ");
+                    }
+                    else
+                    {
+                        sb.Append(Drow());
+                    }
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 출력예시
+        /// *****
+        /// ****
+        /// ***
+        /// **
+        /// *
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public string Number3(int line)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Number3\n====================");
+            for (int y = 0; y < line; y++)
+            {
+                for(int x = line - y; x > 0; x--)
+                {
+                    sb.Append(Drow());
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 출력예시
+        /// *****
+        ///  ****
+        ///   ***
+        ///    **
+        ///     *
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public string Number4(int line)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Number4\n====================");
+            for (int y = 0, star = 5; y < line; y++, star--)
+            {
+                for (int x = 0; x < line; x++)
+                {
+                    if (line - x <= star)
+                    {
+                        sb.Append(Drow());
+                    }
+                    else
+                    {
+                        sb.Append(" ");
+                    }
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
         }
     }
 }
--- 01_Loop/obj/Debug/net8.0/01_Loop.AssemblyInfo.cs
+++ 01_Loop/obj/Debug/net8.0/01_Loop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_Loop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6d5555934e9135b084f26211f52e9228a4edb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_Loop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_Loop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
